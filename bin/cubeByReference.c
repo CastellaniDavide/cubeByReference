6a1aaf3aec015a30ae7487a93a060e1763166b49
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-09 08:25:57 +0200

    Created bin/template.c

---
+++ bin/cubeByReference.c
@@ -0,0 +1,22 @@
+/**
+ * @file cubeByReference.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Create cube function with reference in c
+ *
+ * @ingroup cubeByReference
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("cubeByReference");
+	return 0;
+}
